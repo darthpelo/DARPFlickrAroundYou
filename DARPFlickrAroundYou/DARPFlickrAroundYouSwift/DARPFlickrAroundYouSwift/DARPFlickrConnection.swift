b3aa84bb4104e566285c3abb8a33b57e393139ed
darthpelo <darthpelo@gmail.com>
2014-08-26 10:05:28 +0200

    Add Alamofire (with submodule)

---
+++ DARPFlickrAroundYou/DARPFlickrAroundYouSwift/DARPFlickrAroundYouSwift/DARPFlickrConnection.swift
@@ -0,0 +1,13 @@
+//
+//  DARPFlickrConnection.swift
+//  DARPFlickrAroundYouSwift
+//
+//  Created by Alessio Roberto on 25/08/14.
+//  Copyright (c) 2014 Alessio Roberto. All rights reserved.
+//
+
+import Foundation
+
+class Manager {
+    
+}
